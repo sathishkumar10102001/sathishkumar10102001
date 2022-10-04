93b0d4d0e816032802d339aba53f2d650e6307fb
sathish kumar <114742981+sathishkumar10102001@users.noreply.github.com>
2022-10-04 07:14:15 +0530

    Delete re_split.py

--- re_split.py
+++
@@ -1,9 +0,0 @@
-import re
-result=re.split(r'a','analytics')
-print(result)
-
-#**************************************************
-
-import re
-result=re.split(r'a','Analytics sathish',maxsplit=2)
-print(result)
